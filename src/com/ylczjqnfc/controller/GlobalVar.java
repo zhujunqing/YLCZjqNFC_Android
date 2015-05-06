5056c9341d8f3f5e055835aa60d221fbaf6b1f5d
zhujunqing <zhujunqing@163.com>
2015-05-06 18:27:42 +0800

    增加几个用于控制的全局变量

--- src/com/ylczjqnfc/controller/GlobalVar.java
+++ src/com/ylczjqnfc/controller/GlobalVar.java
@@ -2,4 +2,24 @@ package com.ylczjqnfc.controller;
 
 public class GlobalVar {
 
+	public static final int ERROR = 0;
+
+	public static final int SUCCESS = 1;
+
+	public static final String TAG = "YLCZjqNFC_Android";
+
+	public static final boolean isExit = false;
+
+	// LogUtil
+	public static boolean isDebug = true;
+
+	public static boolean isVerbose = true;
+
+	public static boolean isInformation = true;
+
+	public static boolean isWarning = true;
+
+	public static boolean isError = true;
+	
+	public static String imgDownloadUrl = null;
 }
